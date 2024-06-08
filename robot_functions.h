63fbbbfb2f1ab9815d90168a9f7203735c7d3535
Faseeh Irfan <43595947+faseehirfan@users.noreply.github.com>
2024-06-08 13:48:16 -0700

    add robot_functions.h

---
+++ robot_functions.h
@@ -0,0 +1,12 @@
+#ifndef ROBOT_FUNCTIONS_H
+#define ROBOT_FUNCTIONS_H
+
+#include "vex.h"
+
+void pre_auton();
+int intialSearch();
+bool searchCube();
+void intake(int vel);
+void driveStop();
+
+#endif
