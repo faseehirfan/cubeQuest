f39095f63c3386cbf3cc66094ad6fb6a187bab7d
Faseeh Irfan <43595947+faseehirfan@users.noreply.github.com>
2024-06-08 13:54:19 -0700

    add robot_config.h

---
+++ robot_config.h
@@ -0,0 +1,15 @@
+#ifndef ROBOT_CONFIG_H
+#define ROBOT_CONFIG_H
+
+#include "vex.h"
+
+// Define hardware objects
+extern vex::brain Brain;
+extern vex::motor rMotor;
+extern vex::motor lMotor;
+extern vex::gyro  myGyro;
+extern vex::sonar sonar_L;
+extern vex::sonar sonar_Top;
+extern vex::sonar sonar_Cube;
+
+#endif
